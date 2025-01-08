9f3d0e89256868328c009125f1650ef6f106bd4a
Administrator <Administrator@i7E-30>
2025-01-08 12:39:20 +0900

    3

--- src/d0108/Lotto3.java
+++ src/d0108/Lotto3.java
@@ -1,5 +1,21 @@
 package d0108;
 
-public class Lotto3 {
+import java.util.Random;
 
+public class Lotto3 {
+	public static void main(String[] args) {
+		int[] nums = new int[3];
+		Random r = new Random();
+		
+		
+		
+		nums[0] = r.nextInt(10) + 1;
+		nums[1] = r.nextInt(10) + 1;
+		nums[2] = r.nextInt(10) + 1;
+		
+		System.out.println(nums[0]);
+		System.out.println(nums[1]);
+		System.out.println(nums[2]);
+		
+	}
 }
